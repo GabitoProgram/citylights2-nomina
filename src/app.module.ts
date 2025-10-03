b802fbffc7922a511c9a2c77a740065888abafed
GabitoProgram <153461149+GabitoProgram@users.noreply.github.com>
2025-10-02 20:53:12 -0400

    Temporarily disable ReportesModule to fix build

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TrabajadorModule } from './trabajador/trabajador.module';
 import { NominaModule } from './nomina/nomina.module';
 import { PagarModule } from './pagar/pagar.module';
-import { ReportesModule } from './reportes/reportes.module';
+// import { ReportesModule } from './reportes/reportes.module';
 import { PagoController } from './pago/pago.controller';
 import { PagoService } from './pago/pago.service';
 import { PdfController } from './pdf/pdf.controller';
@@ -12,7 +12,7 @@ import { FacturaNominaService } from './factura/factura-nomina.service';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [TrabajadorModule, NominaModule, PagarModule, ReportesModule],
+  imports: [TrabajadorModule, NominaModule, PagarModule], // ReportesModule
   controllers: [PagoController, PdfController, FacturaNominaController],
   providers: [PagoService, PdfService, FacturaNominaService, PrismaService],
 })
